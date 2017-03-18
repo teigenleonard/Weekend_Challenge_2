30f309d2fb75aee41654876052e47629e7bfda13
Teigen Leonard <teigenl@gmail.com>
2017-03-18 17:04:24 -0500

    add eventListeners

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,20 @@
 $(document).ready( function(){
   console.log('jquery is up');
+
+  eventListeners();
+  appendDon();
+
+});
+
+function eventListeners(){
+  $( "#calculate" ).on( 'click', function(){
+    console.log('button on');
+
+  })
+
+function appendDom(){
+  console.log('appendDom on');
+  // $( 'answers' ).empty();
+}
+
 }
