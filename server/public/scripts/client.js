2d6918a198883c66051238c8bdac019bfcaca1d4
Teigen Leonard <teigenl@gmail.com>
2017-03-18 17:26:13 -0500

    add appendDom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,19 +2,21 @@ $(document).ready( function(){
   console.log('jquery is up');
 
   eventListeners();
-  appendDon();
+  appendDom();
 
-});
+});//end doc ready
 
 function eventListeners(){
   $( "#calculate" ).on( 'click', function(){
     console.log('button on');
 
-  })
+  });//end eventListeners
 
-function appendDom(){
+function appendDom(answers){
   console.log('appendDom on');
   // $( 'answers' ).empty();
-}
+  for (var i = 0; i < answers.length; i++) {
+    $( '#answers' ).append( answers[i] );
+  }//where is answers array?
 
-}
+}//end appendDom
