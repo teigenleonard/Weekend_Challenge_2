23128a1e56bb146599e8eb864dfef1c7dc5923c4
Teigen Leonard <teigenl@gmail.com>
2017-03-20 08:02:08 -0500

    wow what a commit totes didnt keep up on these and final

--- server/app.js
+++ server/app.js
@@ -1,20 +1,63 @@
 //globals and requires
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
+var path = require('path');
+var bodyParser = require('body-parser');
 var port = 5000;
 
 //uses
-app.use ( express.static( 'server/public') );
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //get
-app.get( '/', function( req, res ) {
-  res.sendFile( path.resolve( 'server/public/views/index.html' ) );
-  console.log('app.get');
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('server/public/views/index.html'));
+    console.log('app.get');
+});
+
+//POST
+app.post('/calculator', function(req, res) {
+    console.log('Rock City!');
+    console.log( req.body );
+    var input = req;
+    calculationStation(input.x, input.y, input.type);
+    res.send(answer);
+    res.sendStatus(200);
 });
 
 //listen
-app.listen( port, function() {
+app.listen(port, function() {
     console.log('listening');
 });
+
+function calculationStation(x, y, type) {
+    var answer = 0
+    console.log('calculationStation');
+    if ( 'add' ) {
+        return (answer = parseInt(x) + parseInt(y));
+    } else if ( 'subtract' ) {
+        return (answer = parseInt(x) - parseInt(y));
+    } else if ( 'multiply' ) {
+        return (answer = parseInt(x) * parseInt(y));
+    } else if ( 'divide' ) {
+        return (answer = parseInt(x) / parseInt(y));
+    };
+} //end calculationStation
+
+// function add(x, y) {
+//     return (x + y);
+// } //end add
+//
+// function subtract(x, y) {
+//     return (x - y);
+// } //end subtract
+//
+// function multiply(x, y) {
+//     return (x * y);
+// } //end multiply
+//
+// function divide(x, y) {
+//     return (x / y);
+// } //end divide
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,22 +1,75 @@
-$(document).ready( function(){
-  console.log('jquery is up');
+var type = 0
 
-  eventListeners();
-  appendDom();
 
-});//end doc ready
+$(document).ready(function() {
+    console.log('jquery is up');
 
-function eventListeners(){
-  $( "#calculate" ).on( 'click', function(){
-    console.log('button on');
-  });
-}//end eventListeners
+    eventListeners();
 
-function appendDom( answers ){
-  console.log( 'appendDom on' );
-  $( 'answers' ).empty();
-  for (var i = 0; i < answers.length; i++) {
-    // $( '#answers' ).append( answers[i] );
-  }//where is answers array?
-}
-//end appendDom
+    $("#calculate").on('click', function() {
+        console.log('button on');
+
+        var input = {};
+        input.x = $('#valueX').val();
+        input.y = $('#valueY').val();
+        input.type = type;
+        // input.type = operatorType();
+        // input.add = $( '#add' ).val();
+        // input.subtract = $( '#subtract' ).val();
+        // input.multiply = $( '#multiply' ).val();
+        // input.divide = $( '#divide' ).val();
+
+        $.ajax({
+            type: 'POST',
+            url: '/calculator',
+            data: input,
+            success: function(response) {
+                console.log('Detroit');
+                appendDom(response);
+            }
+        });
+    });
+
+
+}); //end doc ready
+
+function eventListeners() {
+    $('#add').on('click', function() {
+        console.log('add is on');
+        return ( type = 'add' );
+    }); //end add
+
+    $('#subtract').on('click', function() {
+        console.log('subtract is on');
+        return ( type = 'subtract' );
+    }); //end subtract
+
+    $('#multiply').on('click', function() {
+        console.log('multiply is on');
+        return ( type = 'multiply' );
+    }); //end multiply
+
+    $('#divide').on('click', function() {
+        console.log('divide is on');
+        return ( type = 'divide' );
+    }); //end divide
+
+    $('#clear').on('click', function() {
+        console.log('clear is on');
+        $('.enterValues').empty();
+        // $( '#valueY' ).empty();
+        $('.answers').empty();
+
+    }); //end clear
+
+
+} //end eventListeners
+
+function appendDom(answer) {
+    console.log('appendDom on');
+    $('.answers').empty();
+    //     for (var i = 0; i < answers.length; i++) {
+    $('.answers').append('<div>' + answer + '</div>');
+    // } //where is answers array?
+    //must use parseInt befre printing to the DOM
+} //end appendDom
